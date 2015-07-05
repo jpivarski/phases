933fc5bc4767fdefcb9deafd307a0f62f4dcdbd1
Jim Pivarski <jpivarski@gmail.com>
2015-07-05 04:46:56 -0500

    added lots of comments

--- src/main/scala/io/github/jpivarski/phases.scala
+++ src/main/scala/io/github/jpivarski/phases.scala
@@ -6,6 +6,8 @@ import scala.reflect.macros.Context
 import scala.annotation.StaticAnnotation
 
 package object phases {
+  // only one annotation/macro in this package: "declare"; the rest are fake (syntax-only) annotations that get removed before compilation
+
   class declare extends StaticAnnotation {
     def macroTransform(annottees: Any*): Any = macro declare_impl
   }
@@ -13,17 +15,24 @@ package object phases {
   def declare_impl(c: Context)(annottees: c.Expr[Any]*): c.Expr[Any] = {
     import c.universe._
 
+    // incidentally, this is a purely functional algorithm... it just worked out that way
+
+    // get (what we think is the) class definition and maybe its companion object (for static declarations)
     val companionPair = annottees.map(_.tree).toList
     val classDefList = companionPair collect {case x: ClassDef => x}
 
+    // make sure this annotation is applied to a class
     if (classDefList.size != 1)
       c.error(c.enclosingPosition, "@phases.declare can only be used on classes")
     val classDef = classDefList.head
     val ClassDef(_, className, _, _) = classDef
 
+    // if there's no companion object, make one
     val companionObjectDef = companionPair.collect({case x: ModuleDef => x}).
       headOption.getOrElse(q"object ${newTermName(className.toString)}")
 
+    // walk through the annotation's arguments to find the transitions
+    // they should have the form State1 -> State2 and always go between distinct states (no reflexive)
     val transitions =
       c.prefix.tree match {
         case Apply(_, args) => args map {
@@ -41,8 +50,11 @@ package object phases {
     if (transitions.isEmpty)
       c.error(c.enclosingPosition, "@phases.declare requires at least one transition")
 
+    // the phase names are distinct beginning or endpoints of the transitions
+    // by the above logic, we know that there are at least two of these
     val phaseNames = (transitions.map(_._1) ++ transitions.map(_._2)).distinct
 
+    // ClassParam and classParams keep track of the class's parameter list for later use in making transition functions (only; not used for anything else)
     case class ClassParam(flags: FlagSet, privateWithin: Name, partitionedAnnotations: (List[Tree], List[Tree]), paramName: TermName, tpt: Tree, rhs: Tree) {
       def has(phase: String) = (partitionedAnnotations._1.isEmpty)  ||  (partitionedAnnotations._1 exists {
         case Apply(Select(New(x), _), Nil) if (phase == x.toString) => true
@@ -69,17 +81,22 @@ package object phases {
         }}} flatten
     }
 
+    // for making transition functions between phases (this is MORE difficult to express as a quasiquote than a raw AST)
     def makeTransitionMethod(from: String, to: String): Tree = {
       val methodArgs = classParams filter {x => x.has(to)  &&  !x.has(from)} map {_.toParameter}
       val constructorArgs = classParams filter {x => x.has(to)} map {_.toArgument}
       DefDef(Modifiers(), newTermName("to" + to), List(), List(methodArgs), TypeTree(), Apply(Select(New(Select(Ident(newTermName(className.toString)), newTypeName(to))), nme.CONSTRUCTOR), constructorArgs))
     }
 
-    class ClassDefFilterer(phaseToKeep: Option[String]) extends Transformer {
+    // the main Transformer class for removing fake annotations and definitions that are (fake-)annotated for a subclass
+    // when phasesToKeep is None, we're building the superclass (no phases)
+    // when phasesToKeep is Some(phase), we're building a subclass (one phase)
+    class ClassDefTransformer(phaseToKeep: Option[String]) extends Transformer {
       val getPhases = {
         case Apply(Select(New(x), _), Nil) if (phaseNames contains x.toString) => x.toString
       }: PartialFunction[Tree, String]
 
+      // ValDefs can be parameters, vals, or vars; this function handles a variety of cases with boolean switches
       def transformValDef(valDef: ValDef, includeGeneral: Boolean, includeSpecific: Boolean): Tree = {
         val phases = valDef.mods.annotations collect getPhases
         val otherAnnotations = valDef.mods.annotations filter {!getPhases.isDefinedAt(_)}
@@ -100,12 +117,19 @@ package object phases {
           EmptyTree
       }
 
+      // the <init> methods for subclasses are very simple; however, this is still easier to express as an AST than a quasiquote
       def transformInit(tree: Tree, args: List[String]): Tree = tree match {
         case Block(Apply(fun, _) :: Nil, expr) => transform(Block(Apply(fun, args map {x => Ident(newTermName(x))}) :: Nil, expr))
         case x => transform(tree)
       }
 
+      // the main transform function
       override def transform(tree: Tree): Tree = tree match {
+        // several things happen to the ClassDef:
+        //   (1) constructor code (all non ValDef at this level) is removed from subclasses (to avoid double-execution when they call their superclass's constructor)
+        //   (2) transition methods are added
+        //   (3) subclasses get named after the phase they represent
+        //   (4) subclasses point to the superclass as their parent
         case ClassDef(mods, name, tparams, Template(parents, self, body)) =>
           val basicTransform = body map {transform(_)} filter {_ != EmptyTree}
 
@@ -122,17 +146,19 @@ package object phases {
           ClassDef(
             transformModifiers(mods),
             phaseToKeep match {
-              case None => name
-              case Some(phase) => newTypeName(phase)
+              case None => name                       // the superclass should keep its original name
+              case Some(phase) => newTypeName(phase)  // the subclasses should be named after their phase
             },
             transformTypeDefs(tparams),
             Template(phaseToKeep match {
-              case None => parents
-              case Some(phase) => List(Ident(name))
+              case None => parents                    // the superclass should keep its original list of superclasses
+              case Some(phase) => List(Ident(name))   // the subclasses should point to the superclass
             }, transformValDef(self), transformedBody))
 
+        // the ValDef case is already handled in the transformValDef method above
         case x: ValDef => transformValDef(x, true, phaseToKeep != None)
 
+        // DefDefs are function definitions, including the constructor (named <init>).
         case DefDef(mods, name, tparams, vparamss, tpt, rhs) =>
           val phases = mods.annotations collect getPhases
           val otherAnnotations = mods.annotations filter {!getPhases.isDefinedAt(_)}
@@ -159,21 +185,28 @@ package object phases {
           else
             EmptyTree
 
+        // all other cases refer to the general Transformer for a simple walk
         case _ => super.transform(tree)
       }
     }
 
-    val superclassDef = (new ClassDefFilterer(None)).transform(classDef)
-    val phaseDefs = phaseNames map {n => (new ClassDefFilterer(Some(n))).transform(classDef)}
+    // transform the original class to make the superclass
+    val superclassDef = (new ClassDefTransformer(None)).transform(classDef)
 
+    // transform the original class to make subclasses, one for each phase
+    val phaseDefs = phaseNames map {n => (new ClassDefTransformer(Some(n))).transform(classDef)}
+
+    // insert the subclasses into the companion object so that they are statically declared
     val companionWithSubclasses = {
       val ModuleDef(mods, name, Template(parents, self, body)) = companionObjectDef
       ModuleDef(mods, name, Template(parents, self, body ++ phaseDefs))
     }
 
-    println(superclassDef)
-    println(companionWithSubclasses)
+    // // optionally print out what we've done
+    // println(superclassDef)
+    // println(companionWithSubclasses)
 
+    // and send it to the Scala compiler
     c.Expr[Any](Block(List(superclassDef, companionWithSubclasses), Literal(Constant(()))))
   }
 }
--- src/test/scala/phases.scala
+++ src/test/scala/phases.scala
@@ -11,10 +11,16 @@ import test.scala._
 
 @RunWith(classOf[JUnitRunner])
 class PhasesSuite extends FlatSpec with Matchers {
+
+  // an example: Meteoroids (rocks in space), Meteors (falling rocks), and Meteorite (rocks on the ground)
+  // are all rocks from space ("Astroliths?")
+  // Meteoroids can become Meteors and Meteors can become Meteorites, but those are the only allowed transitions
+
   @phases.declare(Meteoroid -> Meteor, Meteor -> Meteorite)
   class Astrolith(composition: String, @Meteoroid orbitalVelocity_km_s: Double, @Meteor val impactDate_ts: Long, @Meteorite var massOfRemnant_kg: Double) {
-    println("constructing " + this.getClass.getName)  // should only happen once per object
+    println("constructing " + this.getClass.getName)  // should only happen once per object: see print-out
 
+    // defined for all phases
     def composition_symb = composition.replace("nickel", "Ni").replace("iron", "Fe")
 
     @Meteoroid
@@ -27,18 +33,30 @@ class PhasesSuite extends FlatSpec with Matchers {
     def massOfRemnant_lb = 2.2046 * massOfRemnant_kg
   }
 
+  // for verifying that the phased objects have the right polymorphism
+  def wantsAstrolith(x: Astrolith) = x
+  def wantsMeteoroid(x: Astrolith.Meteoroid) = x
+  def wantsMeteor(x: Astrolith.Meteor) = x
+  def wantsMeteorite(x: Astrolith.Meteorite) = x
+
+  // first time around, don't use the transitions: just construct everything directly
   "astrolith example" must "work for direct constructors" in {
     val peekskill = new Astrolith("nickel-iron")
     peekskill.composition_symb should be ("Ni-Fe")
+    wantsAstrolith(peekskill)
 
     val peekskill_meteoroid = new Astrolith.Meteoroid("nickel-iron", 14.0)
     peekskill_meteoroid.composition_symb should be ("Ni-Fe")
     peekskill_meteoroid.orbitalVelocity_mi_h should be (31317.02 +- 0.01)
+    wantsAstrolith(peekskill_meteoroid)
+    wantsMeteoroid(peekskill_meteoroid)
 
     val peekskill_meteor = new Astrolith.Meteor("nickel-iron", 718674480000L)
     peekskill_meteor.composition_symb should be ("Ni-Fe")
     peekskill_meteor.impactDate_year should be (1992)
     peekskill_meteor.impactDate_ts should be (718674480000L)
+    wantsAstrolith(peekskill_meteor)
+    wantsMeteor(peekskill_meteor)
 
     val peekskill_meteorite = new Astrolith.Meteorite("nickel-iron", 12.4)
     peekskill_meteorite.composition_symb should be ("Ni-Fe")
@@ -46,17 +64,24 @@ class PhasesSuite extends FlatSpec with Matchers {
     peekskill_meteorite.massOfRemnant_kg should be (12.4 +- 0.1)
     peekskill_meteorite.massOfRemnant_kg *= 0.95   // erosion
     peekskill_meteorite.massOfRemnant_lb should be (25.970188 +- 0.000001)
+    wantsAstrolith(peekskill_meteorite)
+    wantsMeteorite(peekskill_meteorite)
   }
 
+  // next time around, make a Meteoroid and put it through its transitions
   it must "work for transitions" in {
     val peekskill_meteoroid = new Astrolith.Meteoroid("nickel-iron", 14.0)
     peekskill_meteoroid.composition_symb should be ("Ni-Fe")
     peekskill_meteoroid.orbitalVelocity_mi_h should be (31317.02 +- 0.01)
+    wantsAstrolith(peekskill_meteoroid)
+    wantsMeteoroid(peekskill_meteoroid)
 
     val peekskill_meteor = peekskill_meteoroid.toMeteor(718674480000L)
     peekskill_meteor.composition_symb should be ("Ni-Fe")
     peekskill_meteor.impactDate_year should be (1992)
     peekskill_meteor.impactDate_ts should be (718674480000L)
+    wantsAstrolith(peekskill_meteor)
+    wantsMeteor(peekskill_meteor)
 
     val peekskill_meteorite = peekskill_meteor.toMeteorite(12.4)
     peekskill_meteorite.composition_symb should be ("Ni-Fe")
@@ -64,5 +89,7 @@ class PhasesSuite extends FlatSpec with Matchers {
     peekskill_meteorite.massOfRemnant_kg should be (12.4 +- 0.1)
     peekskill_meteorite.massOfRemnant_kg *= 0.95   // erosion
     peekskill_meteorite.massOfRemnant_lb should be (25.970188 +- 0.000001)
+    wantsAstrolith(peekskill_meteorite)
+    wantsMeteorite(peekskill_meteorite)
   }
 }
